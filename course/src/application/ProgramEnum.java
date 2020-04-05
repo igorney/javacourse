ecbe617aa19e757c00a2fb83eff7e31a6aebe01a
igorney <igorneysantos@gmail.com>
2020-04-05 17:48:05 -0300

    Final Order Status

--- course/src/application/ProgramEnum.java
+++ course/src/application/ProgramEnum.java
@@ -1,8 +1,20 @@
 package application;
 
+import entities.Order;
+import entities.enums.OrderStatus;
+
+import java.util.Date;
+
 public class ProgramEnum {
     public static void main(String[] args){
         Order order = new Order(1, new Date(), OrderStatus.PENDING_PAYMENT);
+        System.out.println(order.toString());
+        OrderStatus os;
+        os = OrderStatus.valueOf("PROCESSING");
+        order.setStatus(os);
+        System.out.println(order.toString());
+        os = OrderStatus.DELIVERED;
+        order.setStatus(os);
         System.out.println(order.toString());
     }
 }
